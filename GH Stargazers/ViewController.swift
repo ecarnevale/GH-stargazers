7586391198aff304ef2302b16579b5450d351a3f
Emanuel Carnevale <emanuel.carnevale@gmail.com>
2020-10-11 23:16:56 +0200

    Initial Commit

---
+++ GH Stargazers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GH Stargazers
+//
+//  Created by Emanuel Carnevale on 11/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
